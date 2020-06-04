eec7fe9692b77b688f16a952c0a66fc9f2429add
Leo Liu <leoliuhy@gmail.com>
2020-06-04 10:22:54 -0400

    Directory s4(KNN) finished

---
+++ s3/RegressionExcersice.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+import pickle
+
+data = pd.read_csv()
