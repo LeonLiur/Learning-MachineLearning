110b33c56aa62a1f301cd3da34818a6d43beb0a5
Leo Liu <leoliuhy@gmail.com>
2020-06-09 13:31:32 -0400

    Directory s9 complete, movie review model complete.

--- NeuralNetwork/s9/TextClassification.py
+++ NeuralNetwork/s9/TextClassification.py
@@ -3,9 +3,7 @@ from tensorflow import keras
 
 data = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
-
-print(train_data[0])
+'''(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)'''
 
 word_index = data.get_word_index()
 
@@ -17,18 +15,17 @@ word_index["<UNUSED>"] = 3
 
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
-train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen= 250)
-test_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen= 250)
-
+'''train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen= 250)
+test_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen= 250)'''
 
 def decode_review(text):
     return " ".join([reverse_word_index.get(i, "?") for i in text])
 
-
+'''
 # Building the model
 
 model = keras.Sequential()
-model.add(keras.layers.Embedding(10000,16))
+model.add(keras.layers.Embedding(88000,16))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation="relu"))
 model.add(keras.layers.Dense(1,activation="sigmoid"))
@@ -46,12 +43,38 @@ y_train = train_labels[10000:]
 fitModel = model.fit(x_train,y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val), verbose=1)
 
 result = model.evaluate(test_data, test_labels)
+#Saving the model
+model.save("IMDBModel.h5")'''
 
+model = keras.models.load_model("IMDBModel.h5")
+
+
+def review_encode(s):
+    encoded = [1]
+    for word in s:
+        if word in word_index:
+            encoded.append(word_index[word.lower()])
+        else:
+            encoded.append(2)
+    return encoded
+
+
+with open("LeonReview.txt", encoding="utf-8") as f:
+    for line in f.readlines():
+        nline = line.replace(",", "").replace(".","").replace("(","").replace(")","").replace("\'","").replace("\"","").strip().split(" ")
+        encode = review_encode(nline)
+        encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index["<PAD>"], padding="post", maxlen= 250)
+
+        prediction = model.predict(encode)
+
+        print(line)
+        print(encode)
+        print(prediction[0])
+'''
 for i in range(5):
     test_review =test_data[i]
     predict = model.predict([test_review])
     print("Review: ")
     print(decode_review(test_review))
     print("Prediction: " + str(predict[i]))
-    print("Actual: " + str(test_labels[i]))
-
+    print("Actual: " + str(test_labels[i]))'''
