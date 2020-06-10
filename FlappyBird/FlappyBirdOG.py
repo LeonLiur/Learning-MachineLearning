7924410a7542213c262d42da55ae8474b172e66f
Leo Liu <leoliuhy@gmail.com>
2020-06-10 17:04:13 -0400

    image imported for game Flappy Bird

---
+++ FlappyBird/FlappyBirdOG.py
@@ -0,0 +1,16 @@
+import pygame
+import neat
+import time
+import os
+import random
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
+PIPE_IMGS = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BASE_IMGS = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BG_IMGS = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+
+
